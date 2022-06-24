0a7bb78382c0dcf92b181d5cc4c1e941b7274045
Chris Kamphuis <mail@chriskamphuis.com>
2022-06-24 11:55:46 +0200

    rename to lg

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='log',
+    name='lg',
     version="0.0.1",
     description='Library for loggin in Python',
     author='Chris Kamphuis',
